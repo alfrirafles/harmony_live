261cdbe5d2cd2850f9b1f5a324b7f51b65445876
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-16 23:41:03 +0700

    Added has many messages relationship for guest

--- lib/harmony_live/chat_rooms/guest.ex
+++ lib/harmony_live/chat_rooms/guest.ex
@@ -2,12 +2,14 @@ defmodule HarmonyLive.ChatRooms.Guest do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias HarmonyLive.ChatRooms.Room
+  alias HarmonyLive.ChatRooms.{Room, Message}
   alias HarmonyLive.Users.User
 
   schema "guests" do
     belongs_to :rooms, Room, foreign_key: :room_id
     belongs_to :users, User, foreign_key: :guest_id
+
+    has_many :messages, Message, foreign_key: :sender_id
 
     timestamps()
   end
