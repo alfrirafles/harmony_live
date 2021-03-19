7e59fb9fa8d6fb195f769b1ee68dabb4b610a5f1
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-19 09:28:51 +0700

    Added change management for create and join room pop-ups

--- lib/harmony_live_web/live/page_live.ex
+++ lib/harmony_live_web/live/page_live.ex
@@ -1,9 +1,32 @@
 defmodule HarmonyLiveWeb.PageLive do
   use HarmonyLiveWeb, :live_view
 
+  alias HarmonyLive.ChatRooms.Room
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, query: "", results: %{})}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "Create a Chat Room")
+    |> assign(:room, %Room{})
+  end
+
+  defp apply_action(socket, :join, _params) do
+    socket
+    |> assign(:page_title, "Join a Room")
+    |> assign(:room, %Room{})
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
   end
 
   @impl true
